da0bb7869120cc41c5fe47bf17709f8be440d3ee
raulds-fmtx <rdsflomo03@gmail.com>
2024-05-13 18:04:03 -0500

    Added  GET /*, GET /notes, GET /api/notes, POST /api/notes, app.listen, and all other imports

--- server.js
+++ server.js
@@ -1,7 +1,88 @@
 const express = require("express");
+const generateUniqueId = require("generate-unique-id");
+const fs = require("fs");
 const path = require("path");
-const noteData = require("./db/db.json");
+const notes = require("./db/db.json");
 
 const PORT = 3001;
 
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static("public"));
+
+app.get("/", (req, res) =>
+  res.sendFile(path.join(__dirname, "/public/index.html"))
+);
+
+// GET request for notes page 
+app.get("/notes", (req, res) => {
+  res.sendFile(path.join(__dirname, "/public/notes.html"));
+});
+
+// GET request for notes
+app.get("/api/notes", (req, res) => {
+  console.info(`GET /api/notes`);
+  res.status(200).json(notes);
+});
+
+// POST request to add a note
+app.post('/api/notes', (req, res) => {
+  // Log that a POST request was received
+  console.info(`${req.method} request received to add a note`);
+
+  // Destructuring assignment for the items in req.body
+  const { title, text } = req.body;
+
+  // If all the required properties are present
+  if (title && text) {
+    // Variable for the object we will save
+    const newNote = {
+      title,
+      text,
+      id: generateUniqueId(),
+    };
+
+    // Obtain existing notes
+    fs.readFile("./db/db.json", "utf8", (err, data) => {
+      if (err) {
+        console.error(err);
+      } else {
+        // Convert string into JSON object
+        const parsedNotes = JSON.parse(data);
+
+        // Add a new review
+        parsedNotes.push(newNote);
+
+        // Write updated reviews back to the file
+        fs.writeFile(
+          "./db/db.json",
+          JSON.stringify(parsedNotes, null, 4),
+          (writeErr) =>
+            writeErr
+              ? console.error(writeErr)
+              : console.info("Successfully updated notes!")
+        );
+      }
+    });
+
+    const response = {
+      status: "success",
+      body: newNote,
+    };
+
+    console.log(response);
+
+    // res.json() returns data including a status message indicating the success of the request along with the newly created note data.
+    res.status(201).json(response);
+  } else {
+    // the purpose of the else statement is to allow a way to throw an error if either the title or note is not present.
+    res.status(500).json('Error in saving note');
+  }
+});
+
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT}`)
+);
